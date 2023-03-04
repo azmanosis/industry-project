be710d37342a5a24eff9edd51210aea93ad6bdda
azmanosis <106712529+azmanosis@users.noreply.github.com>
2023-03-04 14:30:56 -0500

    design update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
