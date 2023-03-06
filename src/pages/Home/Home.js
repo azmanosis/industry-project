ba67fe4b03266918ceb92836d076d699bdaa4942
azmanosis <106712529+azmanosis@users.noreply.github.com>
2023-03-06 18:30:36 -0500

    dashboard update

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,19 @@
 import './Home.scss';
+import profile from '../../assets/png/image.svg';
 
 function Home() {
     return (
         <>
+            <div className="dashboard">
+                <div className="dashboard__left">
+                    <p className="dashboard__left--text">Welcome Back, SallyStreamer!
+                    </p>
+                </div>
+                <div className="dashboard__right">
+                    <img className="dashboard__right--image" src={profile} alt="" />
+                    <p className="dashboard__right--text">My Stats</p>
+                </div>
+            </div>
         </>
     )
 }
