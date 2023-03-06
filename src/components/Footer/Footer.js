e4c1008fdd26f96b43447574c0a32f4271fe819f
azmanosis <106712529+azmanosis@users.noreply.github.com>
2023-03-06 16:16:41 -0500

    header and footer complete

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,67 @@
 import './Footer.scss';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/icons/logo.svg';
 
 function Footer() {
     return (
         <>
             <div className="footer">
-                <div className="footer-text">industry project</div>
+                <img src={Logo} className="footer__logo" alt="logo" />
+                <div className="footer__credentials">
+                    <div className="footer__credentials--game">
+                        <Link to="/">
+                            <button className="footer__credentials--game--item">Game Library</button>
+                        </Link>
+                        <Link to="/">
+                            <button className="footer__credentials--game--item">Subscribe</button>
+                        </Link>
+                        <Link to="/">
+                            <button className="footer__credentials--game--item">Redeem Code</button>
+                        </Link>
+                        <Link to="/">
+                            <button className="footer__credentials--game--item">EA app</button>
+                        </Link>
+                        <Link to="/">
+                            <button className="footer__credentials--game--item">About</button>
+                        </Link>
+                    </div>
+                    <div className="footer__credentials--accessibility">
+                        <Link to="/">
+                            <button className="footer__credentials--accessibility--item">accessibility</button>
+                        </Link>
+                        <Link to="/">
+                            <button className="footer__credentials--accessibility--item">help</button>
+                        </Link>
+                    </div>
+                    <div className="footer__credentials--legal">
+                        <Link to="/">
+                            <button className="footer__credentials--legal--item">legal & privacy</button>
+                        </Link>
+                        <Link to="/">
+                            <button className="footer__credentials--legal--item">user agreement</button>
+                        </Link>
+                        <Link to="/">
+                            <button className="footer__credentials--legal--item">privacy and cookie policy(your privacy rights)</button>
+                        </Link>
+                        <Link to="/">
+                            <button className="footer__credentials--legal--item">online service updates</button>
+                        </Link>
+                    </div>
+                    <div className="footer__credentials--service">
+                        <Link to="/">
+                            <button className="footer__credentials--service--item">services</button>
+                        </Link>
+                        <Link to="/">
+                            <button className="footer__credentials--service--item">your privacy choices</button>
+                        </Link>
+                        <Link to="/">
+                            <button className="footer__credentials--service--item">cookie preferences</button>
+                        </Link>
+                    </div>
+                    <div className="footer__credentials--copyright">
+                        <p className="footer__credentials--copyright--legals">2023 electronic arts inc.</p>
+                    </div>
+                </div>
             </div>
         </>
     )
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,34 @@
 import './Header.scss';
 import { Link } from 'react-router-dom';
+import Logo from '../../assets/icons/logo.svg';
 
 function Header() {
     return (
         <div className="header">
-            <div className="header-logo">
+            <div className="header__logo">
                 <Link to="/">
                     <div>
-                        <img alt=""></img>
+                        <img src={Logo} className="header__logo--img" alt=""></img>
                     </div>
                 </Link>
             </div>
-            <div>
-                <Link to="/">
-                    <button id="home">Home</button>
-                </Link>
-                <Link to="adpage">
-                    <button id="adpage">Ad Page</button>
-                </Link>
+            <div className="header__subject">
+                <h2 className="header__subject--creator">
+                    Creator Hub
+                </h2>
+                <input className="header__subject--input" placeholder="Search">
+                </input>
+                <div className="header__subject--link">
+                    <Link to="/">
+                        <button className="header__subject--link--dashboard">dashboard</button>
+                    </Link>
+                    <Link to="/">
+                        <button className="header__subject--link--dashboard">support</button>
+                    </Link>
+                    <Link to="/">
+                        <button className="header__subject--link--dashboard">account</button>
+                    </Link>
+                </div>
             </div>
         </div>
     )
