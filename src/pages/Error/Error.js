61cf9faa4b30c554f74ada0767b5cdf677d749f4
azmanosis <106712529+azmanosis@users.noreply.github.com>
2023-03-09 14:18:54 -0500

    error page and styling

---
+++ src/pages/Error/Error.js
@@ -0,0 +1,15 @@
+import './Error.scss';
+import { Link } from 'react-router-dom';
+
+function Error() {
+    return (
+        <>
+            <div className="error">
+                <p className="error__text">Uh oh, no updates here! But don't worry, take a break and enjoy some stillness. Who knows, you might even hear a pin drop! And while you're at it, head back to the
+                    <Link className="error__link" to="/"> home page </Link>for some exciting content. Who knows what delightful surprises await you there!</p>
+            </div>
+        </>
+    )
+}
+
+export default Error;
